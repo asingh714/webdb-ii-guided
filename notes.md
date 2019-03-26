36e36c580248beba9ba00baff82164a51c36be86
Aman <amsingh714@gmail.com>
2019-03-26 13:45:42 -0400

    able to GET 7 GET specific id

---
+++ notes.md
@@ -0,0 +1,4 @@
+A query builder translates from JS to SQL and back.
+
+ORM = Object Relational Mapper { } <> [] 
+
--- roles/roles-router.js
+++ roles/roles-router.js
@@ -1,28 +1,54 @@
-const router = require('express').Router();
+const knex = require("knex");
 
-router.get('/', (req, res) => {
-  // get the roles from the database
-  res.send('Write code to retrieve all roles');
+const knexConfig = {
+  client: "sqlite3",
+  useNullAsDefault: true,
+  connection: {
+    filename: "./data/roles.db3"
+  }
+};
+
+const db = knex(knexConfig);
+
+const router = require("express").Router();
+
+router.get("/", (req, res) => {
+  // returns a promise that resolves to all records in the table
+  db("roles")
+    .then(roles => {
+      res.status(200).json(roles);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 });
 
-router.get('/:id', (req, res) => {
-  // retrieve a role by id
-  res.send('Write code to retrieve a role by id');
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  db("roles")
+    .where({ id })
+    .first() // because we want the first item in the array.
+    .then(role => {
+      res.status(200).json(role);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 });
 
-router.post('/', (req, res) => {
-  // add a role to the database
-  res.send('Write code to add a role');
+router.post("/", (req, res) => {
+  const role = req.body;
+
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // update roles
-  res.send('Write code to modify a role');
+  res.send("Write code to modify a role");
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // remove roles (inactivate the role)
-  res.send('Write code to remove a role');
+  res.send("Write code to remove a role");
 });
 
 module.exports = router;
